c02d3c6552ae0131927b400b414ba096f8f343fd
oliverstivers <77082348+oliverstivers@users.noreply.github.com>
2024-02-10 10:28:26 -0800

    rhombus

---
+++ a3/script.js
@@ -0,0 +1,101 @@
+
+// var rHeight =5;
+// var colorEven = "orange";
+// var colorOdd = "black";
+// var symbol ="*";
+
+function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
+
+    leftSide(pHeight, pColorEven, pColorOdd, pSymbol);
+    rightSide(pHeight, pColorEven, pColorOdd, pSymbol);
+
+
+}
+
+function leftSide(pHeight, pColorEven, pColorOdd, pSymbol) {
+    var combinedHTML = "";
+
+    // Generate upper left corner
+    for (i = 0; i < pHeight; i++) {
+        combinedHTML += "<p>";
+        // Create spaces before symbols
+        for (k = 0; k < pHeight - i - 1; k++) {
+            combinedHTML += "&nbsp;&nbsp;";
+        }
+        // Create each line on the Rhombus
+        for (j = 0; j <= i; j++) {
+            // Is the position even or odd so we change the color
+            if (j % 2)
+                // even
+                combinedHTML += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+            else
+                // odd
+                combinedHTML += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+        }
+        combinedHTML += "</p>";
+    }
+
+    // Generate upper right corner
+    for (i = 0; i < pHeight; i++) {
+        combinedHTML += "<p>";
+        // Create spaces before symbols
+        for (k = 0; k < i; k++) {
+            combinedHTML += "&nbsp;&nbsp;";
+        }
+        // Create each line on the Rhombus
+        for (j = 0; j < pHeight - i; j++) {
+            // Is the position even or odd so we change the color
+            if (j % 2)
+                // even
+                combinedHTML += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+            else
+                // odd
+                combinedHTML += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+        }
+        combinedHTML += "</p>";
+    }
+
+    document.getElementById("leftSide").innerHTML = combinedHTML;
+}
+
+function rightSide(pHeight, pColorEven, pColorOdd, pSymbol) {
+    var combinedHTML = "";
+
+    // Generate upper left corner
+    for (i = 0; i < pHeight; i++) {
+        combinedHTML += "<p>";
+        // Create spaces before symbols
+        
+        // Create each line on the Rhombus
+        for (j = 0; j <= i; j++) {
+            // Is the position even or odd so we change the color
+            if (j % 2)
+                // even
+                combinedHTML += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+            else
+                // odd
+                combinedHTML += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+        }
+        combinedHTML += "</p>";
+    }
+
+    // Generate upper right corner (mirror of left side)
+    for (i = 0; i < pHeight; i++) {
+        combinedHTML += "<p>";
+        for (j = 0; j < pHeight - i; j++) {
+            // Is the position even or odd so we change the color
+            if (j % 2)
+                // even
+                combinedHTML += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+            else
+                // odd
+                combinedHTML += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+        }
+        combinedHTML += "</p>";
+    }
+
+    document.getElementById("rightSide").innerHTML = combinedHTML;
+}
+
+
+
