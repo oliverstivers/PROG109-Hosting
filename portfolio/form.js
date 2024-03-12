5837ea4f1ff882681ef9ff887afe4666b9ba86f6
oliverstivers <oliverstivers@gmail.com>
2024-03-12 13:58:21 -0700

    work on portfolio

---
+++ portfolio/form.js
@@ -0,0 +1,54 @@
+function isValid() {
+    if (firstname() && email()
+    )
+        return true;
+    else
+        document.getElementById("submiterror").innerHTML = "<p><strong>Error Submitting — See Above</strong></p>";
+    event.preventDefault();
+    return false;
+}
+
+FirstName.addEventListener('blur', firstname, false);
+function firstname() {
+    //1) Create variable
+    var validFirstname = false;
+
+    //2) read value from HTML
+    var firstname = document.getElementById("FirstName").value;
+    var errorMessages = "";
+
+    //3) Do validation
+    if (firstname === "null" || firstname === "" || firstname.length > 20) {
+        errorMessages += "<p>The first name is required and cannot be greater than 20 characters</p>";
+        console.log("First name invalid — length")
+    } else if (firstname.match("^[a-zA-Z ,.'-]+$") === null) {
+        errorMessages += "<p>Invalid caracter in last name (accepts only A-Z, a-z, and ,.'-)</p>";
+        console.log("First name invalid — bad characters")
+    } else {
+        validFirstname = true;
+        console.log("First name valid")
+    };
+
+    //4) Send error message to HTML
+    document.getElementById("fname").innerHTML = errorMessages;
+
+    //5) return status of each field
+    return (validFirstname);
+};
+
+function email() {
+    var userEmail = document.getElementById("Email").value;
+    var atpos = userEmail.indexOf("@");
+    var dotpos = userEmail.lastIndexOf(".");
+    var errorMessages = "";
+    if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= userEmail.length) {
+        // send error message. For example:  
+        errorMessages += "<p>Invalid email</p>";
+        document.getElementById("lname").innerHTML = errorMessages;
+
+    }
+    else {
+        return true; //Or assign the value to a variable. For example validEmail = true
+    }
+
+}
